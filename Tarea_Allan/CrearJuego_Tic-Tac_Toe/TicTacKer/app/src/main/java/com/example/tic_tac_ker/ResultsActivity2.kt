e6b0d3ee33d2c3220c76215744c89e81838f5f95
Traumerei <kerenlarios511@gmail.com>
2025-09-23 10:12:24 -0600

    yy

---
+++ Tarea_Allan/CrearJuego_Tic-Tac_Toe/TicTacKer/app/src/main/java/com/example/tic_tac_ker/ResultsActivity2.kt
@@ -0,0 +1,20 @@
+package com.example.tic_tac_ker
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ResultsActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_results2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
